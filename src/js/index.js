472726f88fd78428036963d52c238c40231c8baa
devisd <89788799+devisd@users.noreply.github.com>
2022-05-19 13:34:42 +0700

    add check tranding

--- src/js/index.js
+++ src/js/index.js
@@ -5,74 +5,71 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 // Notiflix
 import { Notify } from "notiflix/build/notiflix-notify-aio";
-// import API
-import FetchImages from "./fetchPhoto";
-
+//API KEY
+const KEY = "60778458bdbdfa7e14ca7e73fe4a1fef";
+//BASE elements
 const refs = {
   form: document.querySelector("#search-form"),
   galleryList: document.querySelector(".gallery"),
   loadMoreBtn: document.querySelector(".load-more"),
-  fetchPhoto: new FetchImages(),
   gallery: new SimpleLightbox(".gallery a", { loop: true, enableKeyboard: true, docClose: true, }),
   poster_path: 'https://image.tmdb.org/t/p/w500',
 };
 
-refs.loadMoreBtn.setAttribute("disabled", true);
-
 refs.form.addEventListener("submit", onSearch);
 // refs.loadMoreBtn.addEventListener("click", onClick);
 
+// Disable button LOAD-MORE
+refs.loadMoreBtn.setAttribute("disabled", true);
+
+// SEARCH MOVIE FUNCTION
 function onSearch(e) {
   e.preventDefault();
 
+  // Enable button LOAD-MORE
   refs.loadMoreBtn.removeAttribute("disabled");
 
-  refs.fetchPhoto.query = e.currentTarget.elements.searchQuery.value.trim();
-  try {
+  // Form input value
+  const movie = e.currentTarget.elements.searchQuery.value.trim();
 
-  // fetch(`https://api.themoviedb.org/3/search/movie?api_key=60778458bdbdfa7e14ca7e73fe4a1fef&query=${searchName}&page=1`)
-  if (refs.fetchPhoto.query === "") {
-      refs.loadMoreBtn.setAttribute("disabled", true);
-      return Notify.warning("Please enter your request");
-    }
+  // Clear DIV gallery
+  clearMarkup();
 
-    clearMarkup();
-    refs.fetchPhoto.resetPage();
-
-    refs.fetchPhoto.fetchImages()
-      .then(markupPhotoList)
-      .then(renderGallery);
-  } catch {
-    onError();
+  // Check TRANDING or MOVIE
+  if (movie === "") {
+    fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${KEY}`)
+      .then(response => response.json())
+      .then(object => {
+        console.log(object);
+        totalResultCheck(object);
+        return markupPhotoList(object);
+      })
+      .then(renderGallery)
+      .catch(onError);
+  } else {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=${movie}`)
+      .then(response => response.json())
+      .then(object => {
+        console.log(object);
+        totalResultCheck(object);
+        return markupPhotoList(object);
+      })
+      .then(renderGallery)
+      .catch(onError);
   }
 }
 
-// function onClick() {
-//   refs.fetchPhoto.fetchImages()
-//     .then(object => {
-//       checkEndofSearchResult(object);
-//       return markupPhotoList(object);
-//     })
-//     .then(renderGallery);
-// }
+// Check object of Total Result
+function totalResultCheck(object) {
+  if (object.total_results === 0) {
+    refs.loadMoreBtn.setAttribute("disabled", true);
+    return Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+  }
+  refs.loadMoreBtn.removeAttribute("disabled");
+  return Notify.success(`Hooray! We found ${object.total_results} images.`);
+}
 
-// function checkEndofSearchResult(object) {
-//   if (object.hits.length < refs.fetchPhoto.per_page) {
-//     refs.loadMoreBtn.setAttribute("disabled", true);
-//     return Notify.info('We`re sorry, but you`ve reached the end of search results.');
-//   }
-// }
-
-// function totalHitsCheck(object) {
-//   if (object.total === 0) {
-//     refs.loadMoreBtn.setAttribute("disabled", true);
-//     return Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-//   }
-//   refs.loadMoreBtn.removeAttribute("disabled");
-//   Notify.success(`Hooray! We found ${object.totalHits} images.`);
-//   checkEndofSearchResult(object);
-// }
-
+// MARKUP object
 function markupPhotoList(object) {
   return object.results.map(({ poster_path, backdrop_path, title, original_title, genre_ids, vote_average, release_date, }) =>
           `<a class="gallery__item" href="https://image.tmdb.org/t/p/w500${backdrop_path}">
@@ -97,37 +94,19 @@ function markupPhotoList(object) {
     ).join("");
 }
 
+// RENDER FUNCTION
 function renderGallery(markup) {
   refs.galleryList.insertAdjacentHTML("beforeend", markup);
   refs.gallery.refresh();
 }
 
+// CLEAR DIV gallary FUNCTION
 function clearMarkup() {
   refs.galleryList.innerHTML = "";
 }
 
+// ERROR FUNCTION
 function onError() {
   refs.loadMoreBtn.setAttribute("disabled", true);
   return Notify.failure("Oops, that went wrong. Please try again later");
 }
-
-// // ==========================================================================================================
-
-// // Прокрутка страницы
-// // Сделать плавную прокрутку страницы после запроса и отрисовки каждой следующей группы изображений. Вот тебе код подсказка, а разберись в нём самостоятельно.
-
-// // const { height: cardHeight } = document
-// //   .querySelector('.gallery')
-// //   .firstElementChild.getBoundingClientRect();
-
-// // window.scrollBy({
-// //   top: cardHeight * 2,
-// //   behavior: 'smooth',
-// // });
-
-// // ==========================================================================================================
-
-// // Бесконечный скролл
-// // Вместо кнопки «Load more» можно сделать бесконечную загрузку изображений при прокрутке страницы. Мы предоставлям тебе полную свободу действий в реализации, можешь использовать любые библиотеки.
-
-
